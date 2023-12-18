6913f06cc2bca8bf4dbe9282c43683b5d44fae61
ribo <roibeard.mac.unfraidh@gmail.com>
2023-12-18 05:21:33 +0100

    commit b4 phasr

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-      <App />
-  </BrowserRouter>
-  , document.getElementById('root')
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
