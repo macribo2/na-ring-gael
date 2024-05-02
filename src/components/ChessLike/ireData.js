00502d8f828380d614cad7a08d3165bd436d030f
macribo <roibeard.mac.unfraidh@gmail.com>
2024-05-02 08:23:02 +0200

    Press down to exit cdNav.

--- src/components/ChessLike/ireData.js
+++ src/components/ChessLike/ireData.js
@@ -12,27 +12,578 @@ const ireData  = {
                             "irishName": "Cathair na Gaillimhe",
                             "englishName": "Galway City"
                         },
+
+                        /*
+                        "Poll an Phúca",
+      "Cill Cais",
+      "An Spidéal ",
+      "An Teach Dóite",
+      "An Ċeaṫrú Rua",
+      "Inis Meáin "
+                        
+        "The Pooka's hole",
+      "Cill Cais",
+      "An Spidéal ",
+      "The burned House",
+      "An Ċeaṫrú Rua",
+      "Inis Meáin "
+                        
+                        */
                         // More locations for Galway...
                     ]
                 },
                 // More counties for Connacht...
-            ]
+
+                {
+                "gaCoName": "Co. An Ċláir",
+                "enCoName": "Co. Clare",
+                "locations":[
+                    {
+                        "irishName":"",
+                        "englishName":"",
+
+                    },
+                    {
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },
+
+                ],
+
+                }
+            ,
+        
+        
+            {
+                "gaCoName": "",
+                "enCoName": "",
+                "locations":[
+                    {
+                        "irishName":"",
+                        "englishName":"",
+
+                    },
+                    {
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },
+
+                ],
+
+                }
+        ,
+        {
+            "gaCoName": "",
+            "enCoName": "",
+            "locations":[
+                {
+                    "irishName":"",
+                    "englishName":"",
+
+                },
+                {
+                    "irishName":"",
+                    "englishName":"",
+
+                },{
+                    "irishName":"",
+                    "englishName":"",
+
+                },{
+                    "irishName":"",
+                    "englishName":"",
+
+                },{
+                    "irishName":"",
+                    "englishName":"",
+
+                },{
+                    "irishName":"",
+                    "englishName":"",
+
+                },
+
+            ],
+
+            }
+,
+{
+    "gaCoName": "",
+    "enCoName": "",
+    "locations":[
+        {
+            "irishName":"",
+            "englishName":"",
+
         },
         {
-            "name": "Leinster",
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },
+
+    ],
+
+    }
+
+        ]
+        },
+        {
+            "enProvence": "Leinster",
+            "gaProvence": "Laighean",
             "counties": [
                 {
                     "name": "Dublin",
+
+                    /* "Fionnġlas",
+      "Cluain Dolcáin",
+      "Cluain Tarbh",
+      "Binn Éadair",
+      "Dún Laoiġaire",
+      "Deilginis"
+      
+       "Finglas - Clear streamlet",
+      "Clondalkin - Dolcan's meadow",
+      "Clontarf - Meadow of Bulls ",
+      "Howth - Éadar's peak",
+      "Dun Laoghaire - Fort of High King Laoghaire Mac Néill",
+      " Dalkey - Thorn Island"
+      
+      */
                     "locations": [
                         {
-                            "name": "Dublin City",
-                            "irishName": "Baile Átha Cliath",
-                            "englishName": "Dublin City"
+                            
+                            "irishName": "",
+                            "englishName": ""
                         },
-                        // More locations for Dublin...
+                        {
+                            
+                            "irishName": "",
+                            "englishName": ""
+                        },{
+                            
+                            "irishName": "",
+                            "englishName": ""
+                        },{
+                            
+                            "irishName": "",
+                            "englishName": ""
+                        },{
+                            
+                            "irishName": "",
+                            "englishName": ""
+                        },{
+                            
+                            "irishName": "",
+                            "englishName": ""
+                        },
                     ]
                 },
                 // More counties for Leinster...
+           
+                {
+/*"co": ,
+    "locations": [     ,"Ráth Ḟearna",,, ,
+      
+    ],
+    "locationsEng": [
+      ,
+      ,
+      ,
+      "Corlis - the pidgeons",
+      
+    ],
+*/
+                    "gaCoName": "Co. Na hIarṁí",
+                    "enCoName": "Co. Westmeath",
+                    "locations":[
+                        {
+                            "irishName": "Baile na gCailleach",
+                            "englishName":"Collinstown - town of the veiled women or town of the hags",
+    
+                        },
+                        {
+                            "irishName":"Ráth Ḟearna",
+                            "englishName":"Raharney - Fearna's ringfort",
+    
+                        },{
+                            "irishName":"Tulaigh an Eallaigh",
+                            "englishName":"Tullynally - hillock of the swans",
+    
+                        },{
+                            "irishName":"Na Colúir",
+                            "englishName":"Corlis - the pidgeons",
+    
+                        },{
+                            "irishName":"Cill Ḃeagáin",
+                            "englishName":"Kilbeggan - church of Bécán, distinguished Irish Saint",
+    
+                        },{
+                            "irishName":"An Muileann gCearr",
+                            "englishName":"Mullingar - the crooked mill",
+    
+                        },
+    
+                    ],
+    
+                    }
+            ,      {
+                "gaCoName": "",
+                "enCoName": "",
+                "locations":[
+                    {
+                        "irishName":"",
+                        "englishName":"",
+
+                    },
+                    {
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },{
+                        "irishName":"",
+                        "englishName":"",
+
+                    },
+
+                ],
+
+                }
+        ,      {
+            "gaCoName": "",
+            "enCoName": "",
+            "locations":[
+                {
+                    "irishName":"",
+                    "englishName":"",
+
+                },
+                {
+                    "irishName":"",
+                    "englishName":"",
+
+                },{
+                    "irishName":"",
+                    "englishName":"",
+
+                },{
+                    "irishName":"",
+                    "englishName":"",
+
+                },{
+                    "irishName":"",
+                    "englishName":"",
+
+                },{
+                    "irishName":"",
+                    "englishName":"",
+
+                },
+
+            ],
+
+            }
+    ,      {
+        "gaCoName": "",
+        "enCoName": "",
+        "locations":[
+            {
+                "irishName":"",
+                "englishName":"",
+
+            },
+            {
+                "irishName":"",
+                "englishName":"",
+
+            },{
+                "irishName":"",
+                "englishName":"",
+
+            },{
+                "irishName":"",
+                "englishName":"",
+
+            },{
+                "irishName":"",
+                "englishName":"",
+
+            },{
+                "irishName":"",
+                "englishName":"",
+
+            },
+
+        ],
+
+        }
+,      {
+    "gaCoName": "",
+    "enCoName": "",
+    "locations":[
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },
+
+    ],
+
+    }
+,      {
+    "gaCoName": "",
+    "enCoName": "",
+    "locations":[
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },
+
+    ],
+
+    }
+,      {
+    "gaCoName": "",
+    "enCoName": "",
+    "locations":[
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },
+
+    ],
+
+    }
+,      {
+    "gaCoName": "",
+    "enCoName": "",
+    "locations":[
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },
+
+    ],
+
+    }
+,      {
+    "gaCoName": "",
+    "enCoName": "",
+    "locations":[
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },
+
+    ],
+
+    }
+,      {
+    "gaCoName": "",
+    "enCoName": "",
+    "locations":[
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },
+        {
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },{
+            "irishName":"",
+            "englishName":"",
+
+        },
+
+    ],
+
+    }
+,
             ]
         },
         // More provinces...
