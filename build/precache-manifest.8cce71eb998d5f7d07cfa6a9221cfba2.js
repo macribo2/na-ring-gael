2ac0120c1eabb7fb3c3dc12ad7ad1f7f7701e093
macribo <roibeard.mac.unfraidh@gmail.com>
2024-09-11 23:08:40 +0200

    Improve English layout for navCD.js.

--- build/precache-manifest.8cce71eb998d5f7d07cfa6a9221cfba2.js
+++ build/precache-manifest.8cce71eb998d5f7d07cfa6a9221cfba2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6b9a8a922db2fb51af05",
+    "revision": "4ba9742fdf657b7aa9b3",
     "url": "/static/css/main.f48e15b0.chunk.css"
   },
   {
-    "revision": "6b9a8a922db2fb51af05",
-    "url": "/static/js/main.82c23680.chunk.js"
+    "revision": "4ba9742fdf657b7aa9b3",
+    "url": "/static/js/main.26847b93.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -988,28 +988,28 @@ self.__precacheManifest = [
     "url": "/static/media/field.fefaccef.png"
   },
   {
-    "revision": "4c08d21eb3bd0552d1d16f9773ad4bd6",
-    "url": "/static/media/fortuna-glass.4c08d21e.png"
-  },
-  {
     "revision": "ee5004be8978604da5e223282c5bbe51",
     "url": "/static/media/sea.ee5004be.png"
   },
   {
-    "revision": "0cdb344e4fbf91bff6d9eaa0cdb88941",
-    "url": "/static/media/mapFrame4.0cdb344e.png"
-  },
-  {
-    "revision": "803e6e686778fd42d5b5ccb0059ef189",
-    "url": "/static/media/wren-hood.803e6e68.png"
+    "revision": "4c08d21eb3bd0552d1d16f9773ad4bd6",
+    "url": "/static/media/fortuna-glass.4c08d21e.png"
   },
   {
     "revision": "4159e8fdaa220a7666d4e619a3429f80",
     "url": "/static/media/wren2b.4159e8fd.png"
   },
   {
+    "revision": "0cdb344e4fbf91bff6d9eaa0cdb88941",
+    "url": "/static/media/mapFrame4.0cdb344e.png"
+  },
+  {
     "revision": "c84c06495852ac730bd653c3aa9bff25",
     "url": "/static/media/wren-code0.c84c0649.png"
+  },
+  {
+    "revision": "803e6e686778fd42d5b5ccb0059ef189",
+    "url": "/static/media/wren-hood.803e6e68.png"
   },
   {
     "revision": "d0fa8e83c08e2f7d1ce2e1cbe8f2e8b6",
@@ -1084,7 +1084,7 @@ self.__precacheManifest = [
     "url": "/static/media/radar2.fc2d582c.ogg"
   },
   {
-    "revision": "36ad85fef1b45402eaed6154c3a260bf",
+    "revision": "5b2d5b2d8096aac88b38c60b674b204a",
     "url": "/index.html"
   }
 ];
